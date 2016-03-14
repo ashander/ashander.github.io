8b4d15a86491b59b8accb8051065537a95187772
Jaime Ashander <jashander@ucdavis.edu>
2016-03-14 15:00:57 -0700

    Gulp pipeline to optimize images

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant'); // $ npm i -D imagemin-pngquant
+var imageminJpegoptim = require('imagemin-jpegoptim');
+
+gulp.task('default', function() {
+  return gulp.src('content/images/**/*')
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [
+      {removeViewBox: false},
+      {cleanupIDs: false}
+    ],
+    use: [pngquant(), imageminJpegoptim()]
+  }))
+  .pipe(gulp.dest('output/images'));
+});
